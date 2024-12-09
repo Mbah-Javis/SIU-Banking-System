f013572b953b2060fda60ce46eebc154436b3630
Laurelle-Tiomela <laurelletiomela22@gmail.com>
2024-12-09 12:13:23 +0100

    update

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("\nSIU Banking System\n")
+import csv
+db_name ='database.csv'
+#read database csv file
+def read_database():
+    with open (db_name,'r') as file:
+        reader=csv.reader(file)
+        return list (reader)
+    
+#save items to csv database
+def save_to_database(funds):
+    with open(db_name,'w',newlines='') as file:
+        writer=csv.writer(file)
+        writer.writerows(funds)
+        
+#funds transfer
+print("welcome to the funds transfer process...")
+
+        
+
