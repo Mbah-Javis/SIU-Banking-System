cef93cff9bc1de05421f56dd66e8fc9829e409d7
Javis <jelvismbah18@gmail.com>
2024-12-09 11:43:21 +0100

    fix database file typo

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("\nSIU Banking System\n")
+import csv
+
+db_name = 'database.csv'
+
+# Read database csv file
+def read_database():
+    with open(db_name, 'r') as file:
+            reader = csv.reader(file)
+            return list(reader)
+
+# Save items to the CSV database.
+def save_to_database(items):
+    with open(db_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(items)
+
