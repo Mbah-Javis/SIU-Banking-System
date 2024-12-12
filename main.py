24d1ac8e66c367cceb5f7a11575df439d1e30892
Happi JN10 <georgemouapi@Georges-MacBook-Pro.local>
2024-12-12 09:25:37 +0100

    update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import csv
+import random
+from datetime import date
 
 db_name = 'database.csv'
 
@@ -13,6 +15,36 @@ def save_to_database(items):
     with open(db_name, 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(items)
+
+def generate_account_number():
+    return ''.join(random.choices('0123456789', k=12))
+
+def get_date():
+    return date.today()
+
+# Create New Bank Account
+def create_new_account():
+    accounts = read_database()
+    account_number = generate_account_number()
+    full_name = input("Enter Your Full Name: ")
+    while True:
+        try:
+            initial_deposit = float(input("Enter Initial Deposit Amount: "))
+            if initial_deposit < 0:
+                print("Amount cannot be negative. Please try again.")
+                continue
+            break
+        except ValueError:
+            print("Invalid amount. Please enter a float or int")
+    date_created = get_date()
+    accounts.append([account_number, full_name, str(date_created), initial_deposit])
+    save_to_database(accounts)
+    print(f"Account created successfully\n")
+    print("-"*60)
+    print(f"{'Account ID':<15} {'Name':<15} {'Date Created':<15} Balance XAF")
+    print("-"*60)
+    print(f"{account_number:<15} {full_name:<15} {str(date_created):<15} {initial_deposit}")
+    print("\n")
 
 def show_balance():
     
@@ -39,15 +71,13 @@ def deposit_funds():
 balance=0
 def get_choice(choice):
     if choice == 1:
-        print("Create New Account")
+        create_new_account()
     elif choice == 2:
         deposit_funds()
-        print("Deposit Funds")
     elif choice == 3:
         print("Withdraw Funds")
     elif choice == 4:
         show_balance()
-        print("Check Balance")
     elif choice == 5:
         print("Transfer Funds")
     elif choice == 6:
@@ -63,7 +93,7 @@ def get_choice(choice):
 # Menu Display
 def menu():
     print("-" * 26)
-    print(f"|{'':<10}Menu{'':<10}|")
+    print(f"{'':<10}Menu{'':<10}")
     print("-" * 26)
     print("1. Create New Account")
     print("2. Deposit Funds")
@@ -77,12 +107,13 @@ def menu():
 
 print("\n")
 print("-" * 44)
-print(f"|{'':<5}WELCOME TO SIU BANKING SYSTEM {'':<5}|")
+print(f"{'':<5}WELCOME TO SIU BANKING SYSTEM {'':<5}")
 print("-" * 44)
 while True:
     try:
         menu()
-        choice = int(input("\nEnter your choice : "))
+        choice = int(input("\nEnter Your Choice : "))
         get_choice(choice)
     except ValueError:
         print("Invalid input. Please enter a number.")
+
