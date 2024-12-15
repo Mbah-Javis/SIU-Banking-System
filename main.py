9d7b5fecb691464204a9503143cfa1bb28adb0de
cherryyuyun76 <cherryyuyun06@gmail.com>
2024-12-15 23:59:09 +0100

    changed the layout in the index file for better readability and structure removed unecessary elements

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import date
 db_name = 'database.csv'
 
 # Read database csv file
-def read_database():
+def wread_database():
     with open(db_name, 'r') as file:
         reader = csv.reader(file)
         return list(reader)
@@ -18,23 +18,32 @@ def save_to_database(items):
 
 
 def withdraw_funds():
-    accounts = read_database()  
-    account_id = input("Enter your Account ID: ")
+    def withdraw_funds():
+    accounts = read_database()
+    account_id = input("Enter your account ID:")
 
     for account in accounts:
         if account[0] == account_id:
-            amount = float(input("Input amount to withdraw: "))
-            
-    print("Account not found")
-    amount=float(input("enter amount to withdraw:"))
-    if amount> accounts[1]:
-        print("amount is greater than account,please enter a different account.")
-    elif amount <= accounts[1]:
-        accounts[1]-=amount
-        save_account_details=(account_id,account[1])
-        print("Succesful withdrawal")
-    else:
-        print("an error occured,please try again.")
+            try:
+                amount = float(input("Enter amount to withdraw: "))
+                if amount <= 0:
+                    print("Amount must be greater than zero. Please try again.")
+                    continue  
+                
+                current_balance = float(account[3])
+                if amount > current_balance:
+                    print("Insufficient funds")
+                else:
+                    account[3] = str(current_balance - amount)
+                    save_to_database(accounts)
+                    print(f"withdrawal of {amount} succesful!")
+                    return
+            except ValueError:
+                print("Invalid amount. Please try again.")
+                return  
+              
+        print("Account not found\n")
+        
     
 
 def generate_account_number():
